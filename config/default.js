286a5feff03567957ff18308a21b39d0f8543605
Ahmed Kamel <ahmed.kamel@gmail.com>
2019-03-25 00:56:19 -0700

    add trim to secrets

--- config/default.js
+++ config/default.js
@@ -14,27 +14,25 @@ module.exports = {
   "orchestrator" : {
     "name"      : "default_orchestrator",
     "swarm"     : {
-      "hostname"  : fs.readFileSync('/run/secrets/swarm/hostname', 'utf-8'),
-      // "protocol"  : "http:",
+      "hostname"  : _.trim(fs.readFileSync('/run/secrets/swarm/hostname', 'utf-8')),
       "protocol"  : 'https',
-      "port"      : fs.readFileSync('/run/secrets/swarm/port', 'utf-8'),
-      "cert"      : fs.readFileSync('/run/secrets/swarm/cert.pem', 'utf-8'),
-      "key"       : fs.readFileSync('/run/secrets/swarm/key.pem', 'utf-8'),
-      "passphrase": fs.readFileSync('/run/secrets/swarm/passphrase', 'utf-8'),
-      "ca"        : fs.readFileSync('/run/secrets/swarm/ca.pem', 'utf-8')
+      "port"      : _.trim(fs.readFileSync('/run/secrets/swarm/port', 'utf-8')),
+      "cert"      : _.trim(fs.readFileSync('/run/secrets/swarm/cert.pem', 'utf-8')),
+      "key"       : _.trim(fs.readFileSync('/run/secrets/swarm/key.pem', 'utf-8')),
+      "passphrase": _.trim(fs.readFileSync('/run/secrets/swarm/passphrase', 'utf-8')),
+      "ca"        : _.trim(fs.readFileSync('/run/secrets/swarm/ca.pem', 'utf-8'))
     },
     "registry" : {
-      "username" : fs.readFileSync('/run/secrets/registry/username', 'utf-8'),
-      "password" : fs.readFileSync('/run/secrets/registry/password', 'utf-8'),
-      "serveraddress" : fs.readFileSync('/run/secrets/registry/serveraddress', 'utf-8')
+      "username" : _.trim(fs.readFileSync('/run/secrets/registry/username', 'utf-8')),
+      "password" : _.trim(fs.readFileSync('/run/secrets/registry/password', 'utf-8')),
+      "serveraddress" : _.trim(fs.readFileSync('/run/secrets/registry/serveraddress', 'utf-8'))
     },
-    // "token" : fs.readFileSync('/run/secrets/token/jwt', 'utf-8')
-    "token" : _.trim(fs.readFileSync('./key/.jwt', 'utf-8'))
+    "token" : _.trim(fs.readFileSync('/run/secrets/token/jwt', 'utf-8'))
   },
   "sandbox" : {
     "loadbalancer" : {
       "protocol" : "http:",
-      "hostname" : fs.readFileSync('/run/secrets/loadbalancer/hostname', 'utf-8')
+      "hostname" : _.trim(fs.readFileSync('/run/secrets/loadbalancer/hostname', 'utf-8'))
     },
     "limits" : {
       "idle"         : ms('10m'),
